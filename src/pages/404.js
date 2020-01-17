542451e99146fbbecbc8a77c3daba1a4b936a131
Alexandre Lima <limaalexguitar@gmail.com>
2020-01-17 19:55:58 -0300

    style components - local e global

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,6 @@
 import React from "react"
 
-<<<<<<< HEAD
 import Layout from "../components/Layout"
-=======
-import Layout from "../components/layout"
->>>>>>> b76acd58fc13c162ad069bbbe43f1aca95cf8692
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
