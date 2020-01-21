1e4bf5e06c211e9c8c03e17f1c19fe4d5b509ffe
Alexandre Lima <limaalexguitar@gmail.com>
2020-01-21 20:48:00 -0300

    algumas correções no tamanho de textos

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -5,15 +5,18 @@ export const PostItemLink = styled(Link)`
     color: #8899a6;
     display: flex;
     text-decoration: none;
+    border-bottom: 1px solid #333;
 
     &:hover {
         color: #1fa1f2;
+    }
+    &:last-child {
+        border: none;
     }
 `;
 
 export const PostWrapper = styled.section`
     align-items: center;
-    border-bottom: 1px solid var(--textPrimary);
     display: flex;
     padding: 2rem 3rem;
     width: 100%;
@@ -23,7 +26,7 @@ export const PostItemTag = styled.div`
     align-items: center;
     background: ${props => props.background};
     border-radius: 50%;
-    color: var(--textPrimary);
+    color: #c5c5c5;
     display: flex;
     font-size: 1.3rem;
     font-weight: 700;
