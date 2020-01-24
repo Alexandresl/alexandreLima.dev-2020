4a4764e570cfa2c23a5e62680c9878fb73ae80b8
Alexandre Lima <limaalexguitar@gmail.com>
2020-01-23 23:11:02 -0300

    integrando a página de posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import Layout from "../components/Layout/layout";
+import SEO from "../components/seo";
+import "../style/post.less";
+
 const BlogPost = ({ data }) => {
     const post = data.markdownRemark;
 
     return (
-        <>
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-        </>
+        <Layout>
+            <SEO title={post.frontmatter.title} />
+            <div className="post">
+                <div classname="container">
+                    <h1>{post.frontmatter.title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+                </div>
+            </div>
+        </Layout>
     );
 };
 
