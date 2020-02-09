660381f3daa85b7d383a74318ee54c7b7ac300b3
Alexandre Lima <alex.lima.dev@gmail.com>
2020-02-09 18:10:31 -0300

    Estilizando página de Search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import * as S from './styled'
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom";
+
+import * as S from "./styled";
+
+const algolia = {
+  appId: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
+};
+
+const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
 
 const Search = () => (
-    <S.title>Search</S.title>
-)
+  <S.SearchWrapper>
+    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+      <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
+      <Stats translations={{ stats(nbHits, timeSpentMs) {
+        return `${nbHits} resultados encontrados em ${timeSpentMs}`
+      }}} />
+      <Hits />
+    </InstantSearch>
+  </S.SearchWrapper>
+);
 
-export default Search
+export default Search;
